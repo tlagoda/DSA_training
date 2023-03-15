c9a96940712b8fa7cfbc271ef2e00a0920f73408
tlagoda <thibautlagoda@hotmail.fr>
2023-03-15 22:19:28 +0100

    add mystring

---
+++ DSA/Arrays and Strings/isUnique.ts
@@ -0,0 +1,7 @@
+/*
+Is Unique ? : Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures? 
+*/
+
+const myString = "hello world!";
+

2ae5aac0804cadebc38967f9aff77fe2a048b0e8
tlagoda <thibautlagoda@hotmail.fr>
2023-03-19 23:44:36 +0100

    feat:string-compression-91

--- DSA/Arrays and Strings/stringCompression.js
+++ DSA/Arrays and Strings/stringCompression.js
@@ -5,6 +5,27 @@ of repeated characters. For example, the string aabcccccaaa would become a2blc5a
 the original string. You can assume the string has only uppercase and lowercase letters (a - z). 
 */
 
-const string1 = "okay"
-const string2 = "oookkaaaaay"
+const string1 = "hello";
+const string2 = "hello woooooorldddddd !!!";
 
+const compressString = (input) => {
+  let compressedString = "";
+  let count = 0;
+
+  for (let i = 0; i < input.split("").length; i++) {
+    count++;
+
+    if (
+      i + 1 >= input.split("").length ||
+      input.split("")[i] !== input.split("")[i + 1]
+    ) {
+      compressedString += input.split("")[i] + `${count}`;
+      count = 0;
+    }
+  }
+
+  return compressedString.length < input.length ? compressedString : input;
+};
+
+console.log(compressString(string1));
+console.log(compressString(string2));

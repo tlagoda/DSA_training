bfbeaa01b449c66ce9c1f7976e500cdb03640e1d
tlagoda <thibautlagoda@hotmail.fr>
2023-03-19 12:57:10 +0100

    add comments and one use case

--- DSA/Arrays and Strings/isUnique.js
+++ DSA/Arrays and Strings/isUnique.js
@@ -1,13 +1,18 @@
 /*
-Is Unique ? : Implement an algorithm to determine if a string has all unique characters. What if you
+Is Unique? : Implement an algorithm to determine if a string has all unique characters. What if you
 cannot use additional data structures? 
 */
 
 const myString = "hello world!";
+const stringUnique = "algorithms";
 
 const hasAllUniqueCharacters = (input) => {
+  // we create an array to store the characters we have already encountered once
   const charAlreadyEncountered = [];
+
+  // when we encounter a character, we store it in the array
   for (const letter of input.split("")) {
+    // if the charCode is already in the array, then the char is not unique
     if (charAlreadyEncountered[letter.charCodeAt()] === false) {
       return false;
     }
@@ -17,3 +22,4 @@ const hasAllUniqueCharacters = (input) => {
 };
 
 console.log(hasAllUniqueCharacters(myString));
+console.log(hasAllUniqueCharacters(stringUnique));

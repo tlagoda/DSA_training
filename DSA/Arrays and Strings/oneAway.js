abd6d1cbd79d4ffd051561fa4730d8cf6aea0ac7
tlagoda <thibautlagoda@hotmail.fr>
2023-03-22 00:11:19 +0100

    feat/one-away-91 implement algorithm

--- DSA/Arrays and Strings/oneAway.js
+++ DSA/Arrays and Strings/oneAway.js
@@ -9,3 +9,65 @@ pales, pale -> true
 pale, bale -> true
 pale, bake -> false 
 */
+
+const string1 = "pow";
+const string2 = "row";
+const string3 = "hello";
+const string4 = "hell";
+const string5 = "abcd";
+const string6 = "abcde";
+const string7 = "javascript";
+
+const areOneEditAway = (string1, string2) => {
+  // we check the length difference
+  if (Math.abs(string1.length - string2.length) > 1) {
+    return false;
+  }
+  // if strings are the same length, we check for replacement
+  else if (string1.length === string2.length) {
+    let oneDifferenceHasBeenEncountered = false;
+    string1.split("").forEach((letter, index) => {
+      // when we encounter one difference, we check if one has already been encountered
+      if (letter !== string2.split("")[index]) {
+        // if so, we return false
+        if (oneDifferenceHasBeenEncountered) {
+          return false;
+        }
+        oneDifferenceHasBeenEncountered = true;
+      }
+    });
+    return true;
+  } else if (string1.length - 1 === string2.length) {
+    // here, we check for insertion
+    return checkInsertionOrDeletion(string1, string2);
+  } else if (string1.length + 1 === string2.length) {
+    //here, for deletion
+    return checkInsertionOrDeletion(string2, string1);
+  } else {
+    return string1 === string2;
+  }
+};
+
+const checkInsertionOrDeletion = (string1, string2) => {
+  let index1 = 0;
+  let index2 = 0;
+  while (index1 < string1.length && index2 < string2.length) {
+    // if two characters are not equals for the second time, we return false
+    // if it's the first time, we shift the index
+    if (string1.split("")[index1] !== string2.split("")[index2]) {
+      if (index1 !== index2) {
+        return false;
+      }
+      index2++;
+    } else {
+      index1++;
+      index2++;
+    }
+  }
+  return true;
+};
+
+console.log(areOneEditAway(string1, string2));
+console.log(areOneEditAway(string3, string4));
+console.log(areOneEditAway(string5, string6));
+console.log(areOneEditAway(string1, string7));

bd3b4b74a77a54da67201e34931f8b7d7f1d4333
tlagoda <thibautlagoda@hotmail.fr>
2023-03-22 21:27:39 +0100

    implementation of Node class

--- Data Structures/LinkedList.js
+++ Data Structures/LinkedList.js
@@ -1,1 +1,9 @@
 // Implementation of a Linked List
+
+class Node {
+    constructor(data, prev, next) {
+        this.data = data
+        this.prev = prev || null
+        this.nex = next || null
+    }
+}

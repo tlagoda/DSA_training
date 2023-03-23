2360a6165473278a0b3510ea4cf1ab5f548588c9
tlagoda <thibautlagoda@hotmail.fr>
2023-03-23 21:45:39 +0100

    append method implementation

---
+++ Data Structures/LinkedList.js
@@ -0,0 +1,35 @@
+// Implementation of a Linked List
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    if (!this.tail) {
+      this.head = this.tail = new Node(value);
+    } else {
+      let oldTail = this.tail;
+      this.tail = new Node(value);
+      oldTail.next = this.tail;
+      this.tail.previous = oldTail;
+    }
+  }
+
+  prepend() {}
+
+  deleteHead() {}
+
+  deleteTail() {}
+
+  search() {}
+}
+
+class Node {
+  constructor(data, prev, next) {
+    this.data = data;
+    this.prev = prev || null;
+    this.nex = next || null;
+  }
+}

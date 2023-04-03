60a4be5c42654e1c3470ec817647483a7a693f80
tlagoda <thibautlagoda@hotmail.fr>
2023-04-03 22:28:03 +0200

    implement HashTable class with constructor

--- src/Data Structures/HashTable.js
+++ src/Data Structures/HashTable.js
@@ -4,10 +4,19 @@
 
 // hash function
 const hash = (key, size) => {
-    let hashedKey = 0
-    for(let i = 0; i < key.length ; i ++) {
-        hashedKey = key.charCodeAt(i)
-    }
-    return hashedKey % size
-}
+  let hashedKey = 0;
+  for (let i = 0; i < key.length; i++) {
+    hashedKey = key.charCodeAt(i);
+  }
+  return hashedKey % size;
+};
 
+class HashTable {
+  constructor() {
+    this.size = 20;
+    this.buckets = Array(this.size);
+    for (let i = 0; i < this.buckets.length; i++) {
+      this.buckets[i] = new Map();
+    }
+  }
+}

fa48c0c37944bf0c203bb6ff270b4f168cbbb14e
thibaut Lagoda <thibaut.lagoda@JPVELOWKS023.elosi.lan>
2023-04-07 11:35:17 +0200

    modify HashTable class

--- src/Data_Structures/HashTable.js
+++ src/Data_Structures/HashTable.js
@@ -2,15 +2,6 @@
     HASH TABLE
 */
 
-// hash function
-const hash = (key, size) => {
-  let hashedKey = 0;
-  for (let i = 0; i < key.length; i++) {
-    hashedKey = key.charCodeAt(i);
-  }
-  return hashedKey % size;
-};
-
 class HashTable {
   constructor() {
     this.size = 20;
@@ -20,14 +11,23 @@ class HashTable {
     }
   }
 
+  // hash function
+  _hash(key, value) {
+    let hashedKey = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashedKey = key.charCodeAt(i);
+    }
+    return hashedKey % this.size;
+  }
+
   insert(key, value) {
-    let index = hash(key, this.size);
+    let index = this._hash(key, this.size);
     // set is a native method of Map
     this.buckets[index].set(key, value);
   }
 
   remove(key) {
-    let index = hash(key, this.size);
+    let index = this._hash(key, this.size);
     // get and delete are native methods of Map
     let deleted = this.buckets[index].get(key);
     this.buckets[index].delete(key);
@@ -36,29 +36,29 @@ class HashTable {
   }
 
   search(key) {
-    let index = hash(key, this.size);
+    let index = this._hash(key, this.size);
     return this.buckets[index].get(key);
   }
 }
 
 // TESTING
 
-const myHashTable = new HashTable()
+const myHashTable = new HashTable();
 
-myHashTable.insert('sky', 'blue')
-myHashTable.insert('grass', 'green')
-myHashTable.insert('mud', 'brown')
-myHashTable.insert('sun', 'yellow')
-myHashTable.insert('moon', 'white')
+myHashTable.insert("sky", "blue");
+myHashTable.insert("grass", "green");
+myHashTable.insert("mud", "brown");
+myHashTable.insert("sun", "yellow");
+myHashTable.insert("moon", "white");
 
-console.log(myHashTable)
-console.log('------------------')
-console.log(myHashTable.search('sun'))
-console.log(myHashTable.search('moon'))
-console.log('------------------')
+console.log(myHashTable);
+console.log("------------------");
+console.log(myHashTable.search("sun"));
+console.log(myHashTable.search("moon"));
+console.log("------------------");
 
-myHashTable.remove('mud')
-console.log(myHashTable)
+myHashTable.remove("mud");
+console.log(myHashTable);
 
 // EXPORT
 
--- src/utils/Data_Structures/HashTable.js
+++ src/utils/Data_Structures/HashTable.js
@@ -2,15 +2,6 @@
     HASH TABLE
 */
 
-// hash function
-const hash = (key, size) => {
-  let hashedKey = 0;
-  for (let i = 0; i < key.length; i++) {
-    hashedKey = key.charCodeAt(i);
-  }
-  return hashedKey % size;
-};
-
 class HashTable {
   constructor() {
     this.size = 20;
@@ -20,14 +11,22 @@ class HashTable {
     }
   }
 
+  _hash(key, value) {
+    let hashedKey = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashedKey = key.charCodeAt(i);
+    }
+    return hashedKey % this.size;
+  }
+
   insert(key, value) {
-    let index = hash(key, this.size);
+    let index = this._hash(key, this.size);
     // set is a native method of Map
     this.buckets[index].set(key, value);
   }
 
   remove(key) {
-    let index = hash(key, this.size);
+    let index = this._hash(key, this.size);
     // get and delete are native methods of Map
     let deleted = this.buckets[index].get(key);
     this.buckets[index].delete(key);
@@ -36,7 +35,7 @@ class HashTable {
   }
 
   search(key) {
-    let index = hash(key, this.size);
+    let index = this._hash(key, this.size);
     return this.buckets[index].get(key);
   }
 }

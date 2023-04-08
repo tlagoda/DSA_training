4ad4712b1e0d92fe6354eae68b0b819110663ba7
tlagoda <thibautlagoda@hotmail.fr>
2023-04-08 23:45:53 +0200

    fix generateLinkedList function

--- src/utils/Data_Structures/LinkedListService.js
+++ src/utils/Data_Structures/LinkedListService.js
@@ -4,9 +4,9 @@ const generateLinkedList = (llSize, maxValue) => {
   const ll = new LinkedList();
   for (let i = 1; i <= llSize; i++) {
     if (i % 2 !== 0) {
-      ll.append(Math.floor(Math.random() * maxValue));
+      ll.append(Math.floor(Math.random() * maxValue) + 1);
     } else {
-      ll.prepend(Math.floor(Math.random() * maxValue));
+      ll.prepend(Math.floor(Math.random() * maxValue) + 1);
     }
   }
   return ll;

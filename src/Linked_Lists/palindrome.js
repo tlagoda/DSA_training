7fd12c2c92a27b47a15e787706f9329fbacb1af5
tlagoda <thibautlagoda@hotmail.fr>
2023-04-09 19:04:08 +0200

    implement palindrome algorithm

--- src/Linked_Lists/palindrome.js
+++ src/Linked_Lists/palindrome.js
@@ -1,3 +1,16 @@
 /*
     Palindrome: Implement a function to check if a linked list is a palindrome. 
 */
+
+import { LinkedList } from "../utils/Data_Structures/LinkedList.js"
+
+const isPalindrome = (linkedList) => {
+    let reversedLinkedList = new LinkedList()
+    let currentNode = linkedList.head
+    console.log(currentNode)
+    while(currentNode) {
+        reversedLinkedList.prepend(currentNode)
+        currentNode = currentNode.next
+    }
+    return reversedLinkedList
+}

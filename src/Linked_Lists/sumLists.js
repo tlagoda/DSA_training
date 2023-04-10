72bc4bf517712aa59c97e4fd92a22e09c187ddb7
tlagoda <thibautlagoda@hotmail.fr>
2023-04-10 12:36:44 +0200

    implement sumLists algorithm

--- src/Linked_Lists/sumLists.js
+++ src/Linked_Lists/sumLists.js
@@ -7,4 +7,30 @@
     Output: 2 -> 1 -> 9. That is, 912.
 */
 
-const sumLists = () => {};
+import { LinkedList } from "../utils/Data_Structures/LinkedList.js";
+
+const sumLists = (linkedList1, linkedList2) => {
+  let node1 = linkedList1.head;
+  let node2 = linkedList2.head;
+  const result = new LinkedList();
+  let extra = 0;
+  while (node1 || node2) {
+    if (node1.data + node2.data + extra >= 10) {
+      result.append(node1.data + node2.data + extra - 10);
+      extra = 1;
+    } else {
+      result.append(node1.data + node2.data + extra);
+    }
+    if (
+      node1.data + node2.data + extra >= 10 &&
+      (node1.next === null || node2.next === null)
+    ) {
+      result.append(("" + (node1.data + node2.data + extra))[0]);
+    }
+    node1 = node1.next;
+    node2 = node2.next;
+  }
+  return result;
+};
+
+

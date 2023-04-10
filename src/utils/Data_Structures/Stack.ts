8068f1eb0ea195d56df49e8b58fa46483f956ed7
tlagoda <thibautlagoda@hotmail.fr>
2023-04-10 22:38:22 +0200

    create Stack.ts

---
+++ src/utils/Data_Structures/Stack.ts
@@ -0,0 +1,32 @@
+/*
+    STACK
+*/
+
+class Stack {
+  storage: {};
+  size: number;
+  constructor() {
+    this.storage = {};
+    this.size = 0;
+  }
+
+  push(element: any): void {
+    this.size++;
+    this.storage[this.size] = element;
+  }
+
+  pop(): any {
+    let removed = this.storage[this.size];
+    delete this.storage[this.size];
+    this.size--;
+    return removed;
+  }
+
+  peek(): any {
+    return this.storage[this.size];
+  }
+}
+
+// EXPORT
+
+export default Stack;

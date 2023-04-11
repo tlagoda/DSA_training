f795a7a308700e1c35001cfe2c4d918434d98f2b
tlagoda <thibautlagoda@hotmail.fr>
2023-04-11 22:03:08 +0200

    finish converting Arrays and Strings folder

--- src/Arrays_and_Strings/isUnique.ts
+++ src/Arrays_and_Strings/isUnique.ts
@@ -3,19 +3,20 @@ Is Unique? : Implement an algorithm to determine if a string has all unique char
 cannot use additional data structures? 
 */
 
-const prompt = require('prompt-sync')({sigint: true})
+import promptSync from 'prompt-sync'
+const prompt = promptSync()
 
-const hasAllUniqueCharacters = (input) => {
+const hasAllUniqueCharacters = (input: string): boolean => {
   // we create an array to store the characters we have already encountered once
   const charAlreadyEncountered = [];
 
   // when we encounter a character, we store it in the array
   for (const letter of input.split("")) {
     // if the charCode is already in the array, then the char is not unique
-    if (charAlreadyEncountered[letter.charCodeAt()] === false) {
+    if (charAlreadyEncountered[letter.charCodeAt(0)] === false) {
       return false;
     }
-    charAlreadyEncountered[letter.charCodeAt()] = false;
+    charAlreadyEncountered[letter.charCodeAt(0)] = false;
   }
   return true;
 };

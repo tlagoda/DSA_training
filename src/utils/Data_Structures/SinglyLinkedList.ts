c59f7ce4a6f4e07e2c91e73255630102be7b8496
tlagoda <thibautlagoda@hotmail.fr>
2023-04-13 11:01:22 +0200

    implement append & preprend methods

--- src/utils/Data_Structures/SinglyLinkedList.ts
+++ src/utils/Data_Structures/SinglyLinkedList.ts
@@ -1,17 +1,47 @@
 // Implementation of a Singly Linked List
 
-class SinglyLinkedList {}
+class SinglyLinkedList {
+  head: SinglyLinkedListNode;
+  tail: SinglyLinkedListNode;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value: number): void {
+    // in case list is empty
+    if (!this.tail) {
+      this.head = this.tail = new SinglyLinkedListNode(value);
+    } else {
+      let oldTail = this.tail;
+      this.tail = new SinglyLinkedListNode(value);
+      oldTail.next = this.tail;
+    }
+  }
+
+  prepend(value: number): void {
+    // in case list is empty
+    if (!this.head) {
+      this.head = this.tail = new SinglyLinkedListNode(value);
+    } else {
+      let oldHead = this.head;
+      this.head = new SinglyLinkedListNode(value);
+      this.head.next = oldHead;
+    }
+  }
+}
 
 class SinglyLinkedListNode {
   data: number;
   next: SinglyLinkedListNode;
 
-  constructor(data: number, next: SinglyLinkedListNode | null) {
+  constructor(data: number, next?: SinglyLinkedListNode | null) {
     this.data = data;
     this.next = next || null;
   }
 
   toString(): string {
-    return `currentData: ${this.data} | nextData: ${this.next?.data}`
+    return `currentData: ${this.data} | nextData: ${this.next?.data}`;
   }
 }

cf99a1c4fed977caaa937c8c46b2a41d7b7d02f3
tlagoda <thibautlagoda@hotmail.fr>
2023-04-14 16:38:53 +0200

    implement sqrtX algorithm

--- src/Leetcode/sqrtX.ts
+++ src/Leetcode/sqrtX.ts
@@ -5,3 +5,14 @@
     You must not use any built-in exponent function or operator.
 */
 
+const squareRoot = (x: number) => {
+  for (let i = 0; i <= x; i++) {
+    if (i * i < x) {
+      continue;
+    } else if (i * i > x) {
+      return i - 1;
+    } else {
+      return i;
+    }
+  }
+};

40b9ade782ae114ccac0d2fa5ed1c776395acb97
tlagoda <thibautlagoda@hotmail.fr>
2023-04-14 14:56:59 +0200

    implement twoSum algorithm

--- src/Leetcode/twoSum.ts
+++ src/Leetcode/twoSum.ts
@@ -6,3 +6,14 @@
     You can return the answer in any order.
 */
 
+const twoSum = (nums: number[], target: number): number[] => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+

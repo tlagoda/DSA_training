30988f4a8ec6bc00bd637fc0f86bc5b931cb0d0b
tlagoda <thibautlagoda@hotmail.fr>
2023-04-16 23:13:43 +0200

    implement mergeTwoListsUsingNode algorithm

--- src/Leetcode/easy/mergeTwoSortedLists.ts
+++ src/Leetcode/easy/mergeTwoSortedLists.ts
@@ -18,7 +18,6 @@ const mergeTwoListsUsingList = (
   let currentNode1 = list1.head;
   let currentNode2 = list2.head;
   let sortedList = new SinglyLinkedList();
-  // find a way to create a node and add
 
   while (currentNode1?.data && currentNode2?.data) {
     if (currentNode1.data > currentNode2.data) {
@@ -26,10 +25,10 @@ const mergeTwoListsUsingList = (
       if (currentNode2.next) {
         currentNode2 = currentNode2.next;
       } else {
-        currentNode2 = null
-        while(currentNode1) {
-            sortedList.append(currentNode1.data)
-            currentNode1 = currentNode1.next
+        currentNode2 = null;
+        while (currentNode1) {
+          sortedList.append(currentNode1.data);
+          currentNode1 = currentNode1.next;
         }
       }
     } else {
@@ -37,10 +36,10 @@ const mergeTwoListsUsingList = (
       if (currentNode1.next) {
         currentNode1 = currentNode1.next;
       } else {
-        currentNode1 = null
-        while(currentNode2) {
-            sortedList.append(currentNode2.data)
-            currentNode2 = currentNode2.next
+        currentNode1 = null;
+        while (currentNode2) {
+          sortedList.append(currentNode2.data);
+          currentNode2 = currentNode2.next;
         }
       }
     }
@@ -50,18 +49,43 @@ const mergeTwoListsUsingList = (
 
 // TESTING
 
-const ll1 = new SinglyLinkedList()
-const ll2 = new SinglyLinkedList()
+console.log("Test mergeTwoListsUsingList:\n");
 
-ll1.append(1)
-ll2.append(2)
-ll2.append(4)
-ll2.append(7)
-ll2.append(10)
-ll1.append(2)
-ll1.append(4)
-ll1.append(4)
-ll1.append(7)
-ll1.append(9)
+const ll1 = new SinglyLinkedList();
+const ll2 = new SinglyLinkedList();
 
-console.log(mergeTwoListsUsingList(ll1,ll2).toString())
+ll1.append(1);
+ll2.append(2);
+ll2.append(4);
+ll2.append(7);
+ll2.append(10);
+ll1.append(2);
+ll1.append(4);
+ll1.append(4);
+ll1.append(7);
+ll1.append(9);
+
+console.log(mergeTwoListsUsingList(ll1, ll2).toString());
+
+const mergeTwoListsUsingNode = (
+  n1: SinglyLinkedListNode,
+  n2: SinglyLinkedListNode
+): SinglyLinkedListNode => {
+  let head = new SinglyLinkedListNode(null);
+  let tail = head;
+
+  while (n1 && n2) {
+    if (n1.data < n2.data) {
+      tail.next = n1;
+      n1 = n1.next;
+    } else {
+      tail.next = n2;
+      n2 = n2.next;
+    }
+
+    tail = tail.next;
+  }
+
+  tail.next = !n1 ? n2 : n1;
+  return head.next;
+};

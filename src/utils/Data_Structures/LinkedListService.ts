3ec4c68f792eb3b4621695ef0f1923b2132f8c11
tlagoda <thibautlagoda@hotmail.fr>
2023-04-16 19:27:01 +0200

    add generateSinglyLinkedList function

--- src/utils/Data_Structures/LinkedListService.ts
+++ src/utils/Data_Structures/LinkedListService.ts
@@ -1,4 +1,17 @@
+import { SinglyLinkedList } from "./SinglyLinkedList.ts";
 import { DoublyLinkedList } from "./DoublyLinkedList.js";
+
+const generateSinglyLinkedList = (llSize: number, maxValue: number): SinglyLinkedList => {
+  const ll = new SinglyLinkedList();
+  for (let i = 1; i <= llSize; i++) {
+    if (i % 2 !== 0) {
+      ll.append(Math.floor(Math.random() * maxValue) + 1);
+    } else {
+      ll.prepend(Math.floor(Math.random() * maxValue) + 1);
+    }
+  }
+  return ll;
+};
 
 const generateDoublyLinkedList = (llSize: number, maxValue: number): DoublyLinkedList => {
   const ll = new DoublyLinkedList();
@@ -12,4 +25,4 @@ const generateDoublyLinkedList = (llSize: number, maxValue: number): DoublyLinke
   return ll;
 };
 
-export { generateDoublyLinkedList };
+export { generateDoublyLinkedList, generateSinglyLinkedList };

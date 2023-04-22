4e8ef3fc4f93d818dbc6a23c882ca82f7638a758
tlagoda <thibautlagoda@hotmail.fr>
2023-04-22 21:36:08 +0200

    add instructions for pascalTriangle

---
+++ src/Leetcode/easy/pascalTriangle.ts
@@ -0,0 +1,11 @@
+/*
+    Given an integer numRows, return the first numRows of Pascal's triangle.
+
+    In Pascal's triangle, each number is the sum of the two numbers directly above.
+
+    Example:
+    Input: numRows = 5
+    Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+*/
+
+const getPascalTriangle = (numRows: number): Array<number[]> => {};

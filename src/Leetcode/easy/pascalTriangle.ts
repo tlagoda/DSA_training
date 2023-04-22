546035c4045c6c4f68287a2e1c308a90ac03be89
tlagoda <thibautlagoda@hotmail.fr>
2023-04-22 22:01:21 +0200

    add testing

--- src/Leetcode/easy/pascalTriangle.ts
+++ src/Leetcode/easy/pascalTriangle.ts
@@ -10,23 +10,30 @@
 
 const getPascalTriangle = (numRows: number): Array<number[]> => {
   const pascalTriangle = [[1]];
-  
+
   for (let i = 1; i < numRows; i++) {
     const row = [];
     let count1 = 0;
     let count2 = 1;
 
-    row.push(1)
+    row.push(1);
 
-    while (pascalTriangle[i-1][count1] && pascalTriangle[i-1][count2]) {
-      row.push(pascalTriangle[i-1][count1] + pascalTriangle[i-1][count2]);
+    while (pascalTriangle[i - 1][count1] && pascalTriangle[i - 1][count2]) {
+      row.push(pascalTriangle[i - 1][count1] + pascalTriangle[i - 1][count2]);
       count1++;
       count2++;
     }
 
-    row.push(1)
-    pascalTriangle.push(row)
+    row.push(1);
+    pascalTriangle.push(row);
   }
-  return pascalTriangle
+  return pascalTriangle;
 };
 
+// TESTING
+console.log("rows = 1");
+console.log(getPascalTriangle(1));
+console.log("\nrows = 3");
+console.log(getPascalTriangle(3));
+console.log("\nrows = 5");
+console.log(getPascalTriangle(5));

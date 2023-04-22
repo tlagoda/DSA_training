ca9fcece1d9ed494ad780983909fda0a3a45c1f2
tlagoda <thibautlagoda@hotmail.fr>
2023-04-22 23:33:38 +0200

    implement getMajorityElement function

--- src/Leetcode/easy/majorityElement.ts
+++ src/Leetcode/easy/majorityElement.ts
@@ -5,3 +5,9 @@
     You may assume that the majority element always exists in the array.
 */
 
+const getMajorityElement = (nums: number[]): number => {
+  return nums.sort()[
+    nums.length % 2 === 0 ? nums.length / 2 : (nums.length + 1) / 2
+  ];
+};
+

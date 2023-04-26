fdd3ca6659b49d5f186efa9328e269f461b09eff
tlagoda <thibautlagoda@hotmail.fr>
2023-04-26 22:47:50 +0200

    implement getFibNumber() function

--- src/Leetcode/easy/fibonacciNumber.ts
+++ src/Leetcode/easy/fibonacciNumber.ts
@@ -1,5 +1,17 @@
 /*
     The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
-    such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+    such that each number is the sum of the two preceding ones, starting from 0 and 1. 
+    That is :
+    F(0) = 0, F(1) = 1
+    F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+    Given n, calculate F(n).
 */
+
+const getFibNumber = (n: number): number => {
+    if(n === 0 || n === 1) {
+        return n === 0 ? 0 : 1
+    }
+    return getFibNumber(n-1) + getFibNumber(n-2)
+};
 

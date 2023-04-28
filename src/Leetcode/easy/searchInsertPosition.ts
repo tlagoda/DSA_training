86915e88fe21db1fc7d2e6c5b5db72959764a9a6
tlagoda <thibautlagoda@hotmail.fr>
2023-04-28 22:46:42 +0200

    implement searchInsertPosition algorithm

--- src/Leetcode/easy/searchInsertPosition.ts
+++ src/Leetcode/easy/searchInsertPosition.ts
@@ -6,3 +6,19 @@
     You must write an algorithm with O(log n) runtime complexity.
 */
 
+const searchInsertPosition = (nums: number[], target: number): number => {
+    let middle = 0;
+    let left = 0;
+    let right = nums.length - 1;
+  
+    // we use binary search algorithm
+    while (left <= right) {
+      middle = Math.floor(left + (right - left) / 2);
+      if (nums[middle] === target) return middle;
+      if (target < nums[middle]) right = middle - 1;
+      else left = middle + 1;
+    }
+    // if target is not found, we return its hypothetical index
+    return left;
+  };
+  

f39e2e5e2baa64607bf3adfbfba7e778a58c2295
tlagoda <thibautlagoda@hotmail.fr>
2023-04-28 22:50:20 +0200

    add testing

--- src/Leetcode/easy/searchInsertPosition.ts
+++ src/Leetcode/easy/searchInsertPosition.ts
@@ -7,18 +7,27 @@
 */
 
 const searchInsertPosition = (nums: number[], target: number): number => {
-    let middle = 0;
-    let left = 0;
-    let right = nums.length - 1;
-  
-    // we use binary search algorithm
-    while (left <= right) {
-      middle = Math.floor(left + (right - left) / 2);
-      if (nums[middle] === target) return middle;
-      if (target < nums[middle]) right = middle - 1;
-      else left = middle + 1;
-    }
-    // if target is not found, we return its hypothetical index
-    return left;
-  };
-  
+  let middle = 0;
+  let left = 0;
+  let right = nums.length - 1;
+
+  // we use binary search algorithm
+  while (left <= right) {
+    middle = Math.floor(left + (right - left) / 2);
+    if (nums[middle] === target) return middle;
+    if (target < nums[middle]) right = middle - 1;
+    else left = middle + 1;
+  }
+  // if target is not found, we return its hypothetical index
+  return left;
+};
+
+// TESTING
+
+const nums = [1, 3, 5, 6];
+const targets = [5, 2, 7];
+for (const target of targets) {
+    console.log(`nums: ${nums} | target: ${target}`)
+    console.log(`index: ${searchInsertPosition(nums, target)}`)
+    console.log('---------------------------------')
+}

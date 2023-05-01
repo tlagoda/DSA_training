baca0af71ddf28cb8e8c3e73af44ce0c0f6b8288
tlagoda <thibautlagoda@hotmail.fr>
2023-05-01 23:38:40 +0200

    add insert() method in BinaryTree class

--- src/utils/Data_Structures/BinaryTree.ts
+++ src/utils/Data_Structures/BinaryTree.ts
@@ -15,10 +15,43 @@ class BinaryTreeNode {
 
 class BinaryTree {
   root: BinaryTreeNode;
-  count: number;
-  min: number;
-  max: number;
-  constructor(value: number) {
+  count: number = 1;
+  min?: number;
+  max?: number;
+
+  constructor(value: number, min: number = 1, max: number = 10) {
     this.root = new BinaryTreeNode(value);
+    this.min = min;
+    this.max = max;
+  }
+
+  insert(value: number): void {
+    if ((this.min && value < this.min) || (this.max && value > this.max)) {
+      console.log("Invalid value.");
+      return;
+    }
+
+    const newNode = new BinaryTreeNode(value);
+    let node = this.root;
+
+    while (true) {
+      if (Math.random() < 0.5) {
+        if (node.left) {
+          node = node.left;
+        } else {
+          node.left = newNode;
+          this.count++;
+          break;
+        }
+      } else {
+        if (node.right) {
+          node = node.right;
+        } else {
+          node.right = newNode;
+          this.count++;
+          break;
+        }
+      }
+    }
   }
 }

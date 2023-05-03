122e45fbe58c179ac90395a522dae10702172892
tlagoda <thibautlagoda@hotmail.fr>
2023-05-03 22:26:23 +0200

    implement isSymmetric function

--- src/Leetcode/easy/symmetricTree.ts
+++ src/Leetcode/easy/symmetricTree.ts
@@ -3,3 +3,20 @@
     (i.e., symmetric around its center).
 */
 
+import { BinaryTreeNode } from "../../utils/Data_Structures/BinaryTree.ts";
+
+const isSymmetric = (root: BinaryTreeNode): boolean => {
+  return areEquals(root.left, root.right);
+};
+
+const areEquals = (node1: BinaryTreeNode, node2: BinaryTreeNode): boolean => {
+  if (node1 === null && node2 === null) return true;
+  if (node1 === null || node2 === null) return false;
+  return (
+    node1.data === node2.data &&
+    areEquals(node1.left, node2.right) &&
+    areEquals(node1.right, node2.left)
+  );
+};
+
+

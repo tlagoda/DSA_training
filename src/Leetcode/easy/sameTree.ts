d0e4d58f05b5da17f321e42980d71f21480a1605
tlagoda <thibautlagoda@hotmail.fr>
2023-05-03 20:00:50 +0200

    implement isSameTree function

--- src/Leetcode/easy/sameTree.ts
+++ src/Leetcode/easy/sameTree.ts
@@ -8,5 +8,15 @@
 import { BinaryTreeNode } from "../../utils/Data_Structures/BinaryTree.ts";
 
 const isSameTree = (root1: BinaryTreeNode, root2: BinaryTreeNode): boolean => {
-    
-}
+  if (root1 === null && root2 === null) {
+    return true;
+  } else if (root1 === null || root2 === null) {
+    return false;
+  } else if (root1.data !== root2.data) {
+    return false;
+  } else {
+    return (
+      isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right)
+    );
+  }
+};

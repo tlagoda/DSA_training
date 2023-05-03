4246523c7367434a2560889eea8c70812e03a86c
tlagoda <thibautlagoda@hotmail.fr>
2023-05-03 19:59:40 +0200

    add instructions

---
+++ src/Leetcode/easy/sameTree.ts
@@ -0,0 +1,12 @@
+/*
+    Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+    Two binary trees are considered the same if they are structurally identical, 
+    and the nodes have the same value.
+*/
+
+import { BinaryTreeNode } from "../../utils/Data_Structures/BinaryTree.ts";
+
+const isSameTree = (root1: BinaryTreeNode, root2: BinaryTreeNode): boolean => {
+    
+}
